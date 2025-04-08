ab2ecad171d0bd8af90dee430fe81dcf0e0634d6
Jhonma <jhonma1238@gmail.com>
2025-04-08 18:45:51 -0500

    tareasJs: ahora confirma si se desea eliminar la tarea

--- src/main/resources/static/tareasJS.js
+++ src/main/resources/static/tareasJS.js
@@ -1,22 +1,49 @@
-async function getTasks(){
+async function  getData(){
     try{
         let res=await fetch("http://localhost:8080/tasks")
+        if (!res.ok){
+            throw new Error("Error al obtener las tareas")
+        }
         let json=await res.json()
         const tabla=document.getElementById("Tabla-tareas")
         json.forEach(task=>{
-            const row=document.createElement("tr")
-            row.innerHTML= `
-            <td>${task.id}</td>
-            <td>${task.titulo}</td>
-            <td>${task.descripcion}</td>
-            <td>${task.completada?"Si":"No"}</td>
-            <td><a href="http://localhost:8080/tasks/delete/${task.id}" class="opc"  >Borrar</a></td>`
-
+            let row =document.createElement("tr")
+            row.innerHTML=`
+        <td>${task.id}</td>
+        <td>${task.titulo}</td>
+        <td>${task.descripcion}</td>
+        <td>${task.completada?"Si":"No"}</td>
+        <td><a href="#" class="opc" data-id="${task.id}">Borrar</a></td>
+      
+        `
             tabla.appendChild(row)
+
+            document.querySelectorAll(".opc").forEach(link =>{
+                link.addEventListener("click",async e =>{
+                    e.preventDefault()
+                    const id=link.getAttribute("data-id")
+                    let conf=confirm("Estas seguro de que deseas borrar la tarea?")
+                    if (conf) {
+                        try {
+                            let res=await fetch(`http://localhost:8080/tasks/delete/${id}`)
+                            if(!res.ok){
+                                throw new Error("Error al eliminar la tarea")
+                            }
+                            alert("Tarea eliminada")
+                            link.closest("tr").remove()
+                        }catch (e){
+                                console.error(e)
+                            }
+
+                    }else{
+                        alert("Operacion cancelada")
+                    }
+                })
+            })
         })
     }catch (e){
-        console.error("Error al obtener tareas: ",e)
+        console.error(e)
     }
 }
-document.addEventListener("DOMContentLoaded",getTasks)
 
+document.addEventListener("DOMContentLoaded",getData)
